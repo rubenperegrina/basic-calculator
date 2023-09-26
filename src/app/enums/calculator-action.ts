3b78b41df0d27d8e289b4dc3366e7db65fa326e4
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-09-26 21:58:48 +0200

    feat: add calculator action enum

---
+++ src/app/enums/calculator-action.ts
@@ -0,0 +1,10 @@
+export enum CalculatorAction {
+    ClearAll = 'A/C',
+    ClearLast = 'C',
+    Calculate = '=',
+    Add = '+',
+    Subtract = '-',
+    Multiply = 'x',
+    Divide = '/',
+    Decimal = '.',
+  }
