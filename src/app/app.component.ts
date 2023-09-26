3595450a4f37cd09d9bdb43eeb661cb1b72f222e
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-09-26 20:14:32 +0200

    Add prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bc-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
